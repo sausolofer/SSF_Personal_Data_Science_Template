f9ee55ae73a25df8fab0becccae1fe7a873a0ec4
Saúl Solorio Fernández <sausolofer@gmail.com>
2024-04-12 13:34:33 -0600

    pre_gen_project.py update

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -8,7 +8,5 @@ print(f"{MESSAGE_COLOR}Almost done!")
 print(f"Initializing a git repository...{RESET_ALL}")
 
 subprocess.call(['git', 'init'])
-subprocess.call(['git', 'add', '*'])
+subprocess.call(['git', 'add', '.'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
-
-print(f"{MESSAGE_COLOR}The beginning of your destiny is defined now! Create and have fun!{RESET_ALL}")
